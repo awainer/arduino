036c91693961a76d83b86d47e73df4967cb7bc08
Maximiliano Bertacchini <maxiberta@gmail.com>
2012-01-08 02:51:41 -0300

    use pin array

--- pov/pov.ino
+++ pov/pov.ino
@@ -1,18 +1,18 @@
 #include "letters.h"
-#define DEBUG 0
+//#define DEBUG
 
-int DELAY=10;
-const int pin_base=0;
-String buffer_in = "Hola mundo!";
-String buffer_p;
+int DELAY = 10;
+const int pins[] = { 8,9,2,3,4,5,6,7 };
+String buffer_p = "Hello world!";
+String buffer_in;
 
 void setup() {
   init_letters();
   //configure pins
-  for(int i=pin_base;i<(pin_base + HEIGHT) ;i++)
-    pinMode(i, OUTPUT);     
+  for(int i=0; i < HEIGHT ;i++)
+    pinMode(pins[i], OUTPUT);     
   //test!
-  pov_test(pin_base,HEIGHT);
+  pov_test();
 #ifdef DEBUG
   Serial.begin(9600);  
   Serial.println("Empiezo setup");
@@ -24,27 +24,26 @@ void setup() {
 #endif
 }
 
-void pov_letra(char l)
-{
+void pov_letra(char l) {
   //  Serial.print(letras[l - offset_a ][1]);
-  for(int i=0;i<WIDTH;i++)
+  for(int i=0; i < WIDTH; i++)
   {
-    for (int j=pin_base, mask=00000001; mask>0; mask<<=1, j++) 
+    for (int j=0, mask=00000001; mask>0; mask<<=1, j++) 
     { //iterate through bit mask
       if (letters[l][i] & mask){ // if bitwise AND resolves to true
-        digitalWrite(j,HIGH); // send 1
+        digitalWrite(pins[j], HIGH); // send 1
       }
       else
       { //if bitwise and resolves to false
-        digitalWrite(j,LOW); // send 0
+        digitalWrite(pins[j], LOW); // send 0
       }
     }
     delay(DELAY);
   }
-  pov_apagar(pin_base,HEIGHT);
+  pov_apagar();
 }
 
-void pov_texto(String t){
+void pov_texto(String t) {
   for (int i=0; i < t.length(); i++){
     pov_letra(t[i]);
     // pov_apagar(pin_base);
@@ -53,21 +52,20 @@ void pov_texto(String t){
   } 
 }
 
-void pov_apagar(int base, int heigh)
-{ 
+void pov_apagar() { 
   // Serial.println("apago");
-  for(int i=0;i<heigh;i++)
-    digitalWrite(base + i, LOW);
+  for(int i=0; i < HEIGHT; i++)
+    digitalWrite(pins[i], LOW);
   // Serial.println("ya apague");
   delay(DELAY);
 }
 
-void pov_test(int base, int heigh){
-  for (int i=pin_base; i < (pin_base+heigh) ; i++)
+void pov_test() {
+  for (int i=0; i < HEIGHT ; i++)
   {
-    digitalWrite(i,HIGH);
+    digitalWrite(pins[i],HIGH);
     delay(200);
-    digitalWrite(i,LOW);
+    digitalWrite(pins[i],LOW);
     delay(200);
   }
 }
@@ -80,22 +78,23 @@ void loop() {
   Serial.println(buffer_in);
 #endif
   pov_texto(buffer_p);
-/*  while(Serial.available() > 0)
-  {
-    //    if(Serial.peek()==13  || Serial.peek()==10)
-    if(Serial.peek()==46)
-    {
-      buffer_p = buffer_in;
-      buffer_in = String();
-      Serial.read();
-    }
-    else
-    {
-      char c=Serial.read();
-      Serial.print("recibo ");
-      Serial.println(c);
-      buffer_in.concat(c);
-    }
-  }*/
+  /*  while(Serial.available() > 0)
+   {
+   //    if(Serial.peek()==13  || Serial.peek()==10)
+   if(Serial.peek()==46)
+   {
+   buffer_p = buffer_in;
+   buffer_in = String();
+   Serial.read();
+   }
+   else
+   {
+   char c=Serial.read();
+   Serial.print("recibo ");
+   Serial.println(c);
+   buffer_in.concat(c);
+   }
+   }*/
 }
+
 
